9b620b44486f09515f60c7057482d9a04e6b2d9e
Goleng <golengmagma@gmail.com>
2022-10-09 17:08:48 +0200

    fixed issues with app

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,6 +1,7 @@
 import sys
 import sqlite3
 import logging
+from logging.config import dictConfig
 
 from flask import (
     Flask, 
@@ -16,6 +17,7 @@ from werkzeug.exceptions import abort
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
@@ -23,19 +25,40 @@ def get_db_connection():
 
 # Function to get a post using its ID
 def get_post(post_id):
+    global db_connection_count 
+    db_connection_count += 1
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
     return post
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stderr',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+db_connection_count = 1
 
 # Define the main route of the web application 
 @app.route('/')
 def index():
+    global db_connection_count 
+    db_connection_count += 1
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
@@ -57,12 +80,16 @@ def post(post_id):
 # Define the About Us page
 @app.route('/about')
 def about():
+    global db_connection_count 
+    db_connection_count += 1
     app.logger.info('About Us page is retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    global db_connection_count 
+    db_connection_count += 1
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -94,7 +121,8 @@ def metrics():
     query = """Select count(*) From posts"""
     connection = get_db_connection()
     post_count = connection.execute(query).fetchone()[0]
-    db_connection_count = connection.total_changes
+    global db_connection_count 
+    db_connection_count += 1
     response = app.response_class(
         response = json.dumps(
             {'db_connection_count': db_connection_count, 
@@ -106,5 +134,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
